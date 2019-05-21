24234defcfbad5b41cbe01b707acc9fa519c0566
TheKillerAboy <36484821+TheKillerAboy@users.noreply.github.com>
2019-05-21 03:59:09 +0200

    21/05/2019

--- COMMONS.pas
+++ COMMONS.pas
@@ -2,12 +2,18 @@ unit COMMONS;
 
 interface
 
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls;
+
+type
+  CompairFunc<K> = reference to function(value1,value2:K):boolean;
+
 type
   MArray<T> = class
   private
-    function FgetIndex(Index:integer):T;   
-    function getLength:integer; 
-    function getCIndex():T;
+    function FgetIndex(Index:integer):T;
+    function getLength:integer;
   public       
     var
       container:TArray<T>;
@@ -15,10 +21,28 @@ type
     constructor Create();overload;
     constructor Create(pre:TArray<T>);overload;
     procedure append(value:T);
-    procedure cIndexRotate;    
+    procedure cIndexRotate;
     property getIndex[Index : integer] : T read FgetIndex;default;
     property len:integer read getLength;
-    
+    function getCIndex():T;
+    function find(value:T):integer;
+    function findIf(value:T;compair:CompairFunc<T>):integer;
+  end;
+
+type
+  MDict<TKey,TValue> = class
+    private
+    function getLength:integer;
+    function GETkeyTOvalue(index:TKey): TVAlue;
+      var
+        keys : MArray<TKey>;
+        values : MArray<TValue>;
+    public
+      constructor Create();
+      property len:integer read getLength;
+      procedure append(key:TKey;value:TValue);
+      property keyTOvalue[Index:TKey] :TValue read GETkeyTOvalue;
+      function valueTOkey(index:TValue): TKey;
   end;
 
 implementation
@@ -52,6 +76,36 @@ begin
   result:=container[Index];
 end;
 
+function MArray<T>.find(value: T): integer;
+var
+i:integer;
+begin
+  for i := 0 to len - 1 do
+  begin
+    if container[i] = value then
+    begin
+      result:=i;
+      exit;
+    end;
+  end;
+  result:=-1;
+end;
+
+function MArray<T>.findIf(value: T; compair: CompairFunc<T>): integer;
+var
+i:integer;
+begin
+  for i := 0 to len - 1 do
+  begin
+    if compair(value,container[i]) then
+    begin
+      result:=i;
+      exit;
+    end;
+  end;
+  result:=-1;
+end;
+
 function MArray<T>.getCIndex: T;
 begin
   result:= container[cIndex];
@@ -60,6 +114,35 @@ end;
 function MArray<T>.getLength: integer;
 begin
   result:= Length(container);
+end;
+
+{ MDict<Key, Value> }
+
+procedure MDict<TKey, TValue>.append(key: TKey; value: TValue);
+begin
+  keys.append(key);
+  values.append(value);
+end;
+
+constructor MDict<TKey, TValue>.Create;
+begin
+  keys := MArray<TKey>();
+  values := MArray<TValue>();
+end;
+
+function MDict<TKey, TValue>.GETkeyTOvalue(index: TKey): TVAlue;
+begin
+  result:=values[keys.find(index)];
+end;
+
+function MDict<TKey, TValue>.getLength: integer;
+begin
+  result:=keys.len;
+end;
+
+function MDict<TKey, TValue>.valueTOkey(index: TValue): TKey;
+begin
+  result:=keys[values.find(index)];
 end;
 
 end.
---
+++ settings_u.dfm
@@ -0,0 +1,31 @@
+object frmSettings: TfrmSettings
+  Left = 0
+  Top = 0
+  Caption = 'Settings'
+  ClientHeight = 283
+  ClientWidth = 510
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object edtSearch: TEdit
+    Left = 8
+    Top = 8
+    Width = 121
+    Height = 21
+    TabOrder = 0
+  end
+  object btnSearch: TButton
+    Left = 144
+    Top = 6
+    Width = 75
+    Height = 25
+    Caption = 'Search'
+    TabOrder = 1
+  end
+end
