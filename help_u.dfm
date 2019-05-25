e10c31c3f042c9c7e618a4d9cfb4711ac28eb8e3
TheKillerAboy <36484821+TheKillerAboy@users.noreply.github.com>
2019-05-25 17:02:26 +0200

    Improved COMMONS, JSON and added help_u geatures and made settings_u use json files

--- help_u.dfm
+++ help_u.dfm
@@ -2,8 +2,8 @@ object frmHelp: TfrmHelp
   Left = 0
   Top = 0
   Caption = 'Help'
-  ClientHeight = 329
-  ClientWidth = 536
+  ClientHeight = 411
+  ClientWidth = 678
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -11,6 +11,77 @@ object frmHelp: TfrmHelp
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
+  OnShow = FormShow
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 8
+    Top = 35
+    Width = 21
+    Height = 13
+    Caption = 'Help'
+  end
+  object Label2: TLabel
+    Left = 485
+    Top = 35
+    Width = 39
+    Height = 13
+    Caption = 'Content'
+  end
+  object tvwHelp: TTreeView
+    Left = 8
+    Top = 54
+    Width = 465
+    Height = 301
+    Indent = 19
+    TabOrder = 0
+    OnChange = tvwHelpChange
+  end
+  object redHelp: TRichEdit
+    Left = 479
+    Top = 54
+    Width = 191
+    Height = 301
+    Font.Charset = ANSI_CHARSET
+    Font.Color = clWindowText
+    Font.Height = -11
+    Font.Name = 'Tahoma'
+    Font.Style = []
+    ParentFont = False
+    TabOrder = 1
+  end
+  object edtSearch: TEdit
+    Left = 8
+    Top = 8
+    Width = 121
+    Height = 21
+    TabOrder = 2
+  end
+  object btnSearch: TButton
+    Left = 144
+    Top = 6
+    Width = 75
+    Height = 25
+    Caption = 'Search'
+    TabOrder = 3
+    OnClick = btnSearchClick
+  end
+  object Panel1: TPanel
+    Left = 8
+    Top = 361
+    Width = 662
+    Height = 42
+    BevelKind = bkTile
+    BevelOuter = bvNone
+    TabOrder = 4
+    object btnMainMain: TButton
+      Left = 575
+      Top = 6
+      Width = 75
+      Height = 25
+      Caption = 'Main Menu'
+      TabOrder = 0
+    end
+  end
 end
--- help_u.pas
+++ help_u.pas
@@ -4,12 +4,28 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, JSON;
+  Dialogs, JSON, ComCtrls, COMMONS, StdCtrls, ExtCtrls;
 
 type
   TfrmHelp = class(TForm)
+    tvwHelp: TTreeView;
+    redHelp: TRichEdit;
+    edtSearch: TEdit;
+    btnSearch: TButton;
+    Panel1: TPanel;
+    btnMainMain: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure btnSearchClick(Sender: TObject);
+    procedure tvwHelpChange(Sender: TObject; Node: TTreeNode);
+    procedure FormShow(Sender: TObject);
   private
-    { Private declarations }
+    procedure AddToTree(Current : MArray<DictObject>; Parent : TTreeNode);
+    var
+      jsonHelp : DictObject;
+      specialHelp : MDict<TTreeNode,string>;
+      dicTree : MDict<string,TTreeNode>;
   public
     { Public declarations }
   end;
@@ -21,4 +37,58 @@ implementation
 
 {$R *.dfm}
 
+procedure TfrmHelp.AddToTree(Current: MArray<DictObject>; Parent : TTreeNode);
+var
+  tmpObj : DictObject;
+  tmpParent : TTreeNode;
+begin
+  for tmpObj in Current.container do
+  begin
+    tmpParent := tvwHelp.Items.AddChild(Parent,tmpObj['title'].toString);
+    dicTree.append(tmpObj['title'].toString,tmpParent);
+    if tmpObj['hasChildren'].toBool then
+    begin
+      AddToTree(tmpObj['content'].toArray,tmpParent);
+    end
+    else
+      specialHelp.append(tmpParent,tmpObj['content'].toString)
+  end;
+end;
+
+procedure TfrmHelp.btnSearchClick(Sender: TObject);
+var
+  searchIndex : integer;
+begin
+  searchIndex := dicTree.keys.findIF(edtSearch.Text,Strings.Equivalent);
+  if searchIndex <> -1 then
+    tvwHelp.Select(dicTree.values[searchIndex]);
+     dicTree.values[searchIndex].Expanded:=true;
+  tvwHelp.SetFocus;
+end;
+
+procedure TfrmHelp.FormCreate(Sender: TObject);
+begin
+  jsonHelp := JSONParser.FileParse('help.json');
+  specialHelp := MDict<TTreeNOde,string>.Create();
+  dicTree := MDict<string,TTreeNode>.Create();
+  AddToTree(jsonHelp['help'].toArray,nil);
+end;
+
+procedure TfrmHelp.FormShow(Sender: TObject);
+var
+  tmpNode:TTreeNode;
+begin
+  for tmpNode in dicTree.values.container do
+    tmpNode.Expanded := false;
+  redHelp.Text := '';
+end;
+
+procedure TfrmHelp.tvwHelpChange(Sender: TObject; Node: TTreeNode);
+var
+  specialIndex : integer;
+begin
+  specialIndex := specialHelp.keys.find(tvwHelp.Selected);
+  if specialIndex <> -1 then
+    redHelp.Text := specialHelp.values[specialIndex];
+end;
 end.
